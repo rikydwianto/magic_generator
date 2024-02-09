e39c1f7ed88f87f49cff9f9b51b2de674311f001
rikydwianto <rikydwianto04@gmail.com>
2024-02-09 11:16:56 +0800

    baru

--- proses/progres/laporan/cek_laporan.php
+++ proses/progres/laporan/cek_laporan.php
@@ -29,13 +29,15 @@ error_reporting(0);
             <div class="col">
                 <div class="form-group">
                     <label for="anggota_masuk">Anggota Masuk:</label>
-                    <input type="text" <?= $disabled ?> oninput="formatAngka(this)" value="<?= $hasil['anggota_masuk'] ?>" class="form-control" name="anggota_masuk">
+                    <input type="text" <?= $disabled ?> oninput="formatAngka(this)"
+                        value="<?= $hasil['anggota_masuk'] ?>" class="form-control" name="anggota_masuk">
                 </div>
             </div>
             <div class="col">
                 <div class="form-group">
                     <label for="anggota_keluar">Anggota Keluar:</label>
-                    <input type="text" <?= $disabled ?> oninput="formatAngka(this)" value="<?= $hasil['anggota_keluar'] ?>" class="form-control" name="anggota_keluar">
+                    <input type="text" <?= $disabled ?> oninput="formatAngka(this)"
+                        value="<?= $hasil['anggota_keluar'] ?>" class="form-control" name="anggota_keluar">
                 </div>
             </div>
             <div class="col">
@@ -43,7 +45,8 @@ error_reporting(0);
                     <label for="nett_anggota">Nett Anggota:</label>
                     <div id="hasil_agt" class="mt-1 <?= $hasil['nett_anggota'] < 0 ? "text-danger" : "text-success" ?>">
                         <?= formatNumber($hasil['nett_anggota']) ?></div>
-                    <input type="hidden" <?= $disabled ?> oninput="formatAngka(this)" readonly value="<?= $hasil['nett_anggota'] ?>" class="form-control" name="nett_anggota">
+                    <input type="hidden" <?= $disabled ?> oninput="formatAngka(this)" readonly
+                        value="<?= $hasil['nett_anggota'] ?>" class="form-control" name="nett_anggota">
                 </div>
             </div>
         </div>
@@ -54,13 +57,16 @@ error_reporting(0);
             <div class="col">
                 <div class="form-group">
                     <label for="naik_par">Outstanding Par Naik:</label>
-                    <input type="text" <?= $disabled ?> oninput="formatAngka(this)" value="<?= formatNumber($hasil['naik_par']) ?>" class="form-control" name="naik_par">
+                    <input type="text" <?= $disabled ?> oninput="formatAngka(this)"
+                        value="<?= formatNumber($hasil['naik_par']) ?>" class="form-control" name="naik_par">
                 </div>
             </div>
             <div class="col">
                 <div class="form-group">
                     <label for="turun_par">Outstanding Par Turun:</label>
-                    <input type="text" <?= $disabled ?> placeholder="3000000" value="<?= formatNumber($hasil['turun_par']) ?>" oninput="formatAngka(this)" class="form-control" name="turun_par">
+                    <input type="text" <?= $disabled ?> placeholder="3000000"
+                        value="<?= formatNumber($hasil['turun_par']) ?>" oninput="formatAngka(this)"
+                        class="form-control" name="turun_par">
                 </div>
             </div>
             <div class="col">
@@ -68,7 +74,8 @@ error_reporting(0);
                     <label for="nett_par">Nett Outstanding Par</label>
                     <div id="hasil" class="mt-1 <?= $hasil['nett_pat'] > 0 ? "text-danger" : "text-success" ?>">
                         <?= formatNumber($hasil['nett_par']) ?></div>
-                    <input type="hidden" <?= $disabled ?> onload="formatAngka(this)" readonly value="<?= formatNumber($hasil['nett_par']) ?>" class="form-control" name="nett_par">
+                    <input type="hidden" <?= $disabled ?> onload="formatAngka(this)" readonly
+                        value="<?= formatNumber($hasil['nett_par']) ?>" class="form-control" name="nett_par">
                 </div>
             </div>
         </div>
@@ -77,16 +84,18 @@ error_reporting(0);
             <?php
             foreach ($pinjamanArray as $kode => $kategori) {
             ?>
-                <div class="col">
-                    <label for="pinjaman<?= $kode ?>"> <?= $kode ?></label>
-                    <input type="number" <?= $disabled ?> value="<?= $json[$kode] ?>" oninput="hitungTotalPinjaman()" class="form-control" id="pinjaman<?= $kode ?>" name="pinjaman[<?= $kode ?>]" min="0">
-                </div>
+            <div class="col">
+                <label for="pinjaman<?= $kode ?>"> <?= $kode ?></label>
+                <input type="number" <?= $disabled ?> value="<?= $json[$kode] ?>" oninput="hitungTotalPinjaman()"
+                    class="form-control" id="pinjaman<?= $kode ?>" name="pinjaman[<?= $kode ?>]" min="0">
+            </div>
             <?php
 
             }
 
             ?>
-            <input type="hidden" readonly id='totalPinjaman' value="<?= $total_pinj ?>" class="form-control" name="pemb_lain">
+            <input type="hidden" readonly id='totalPinjaman' value="<?= $total_pinj ?>" class="form-control"
+                name="pemb_lain">
             <div class="col text-center">
                 <label for="total_pemb"> Total Pemb:</label>
                 <div id="total_pemb"><?= $total_pinj ?></div>
@@ -98,18 +107,20 @@ error_reporting(0);
             <div class="col">
 
                 <div class="form-group">
-                    <label for="agt_tpk">Anggota Cuti <br> <small>Total Anggota cuti yang
+                    <label for="agt_cuti">Anggota Cuti <br> <small>Total Anggota cuti yang
                             dimiliki</small></label>
-                    <input type="number" <?= $disabled ?> value="<?= $hasil['agt_tpk'] ?>" class="form-control" id="agt_tpk" name="agt_tpk">
+                    <input type="number" <?= $disabled ?> value="<?= $hasil['agt_cuti'] ?>" class="form-control"
+                        id="agt_cuti" name="agt_cuti">
 
                 </div>
             </div>
             <div class="col">
 
                 <div class="form-group">
-                    <label for="agt_cuti">Pengajuan Anggota TPK <br> <small>Total Anggota yang diajukan TPK minggu
+                    <label for="agt_tpk">Pengajuan Anggota TPK <br> <small>Total Anggota yang diajukan TPK minggu
                             ini</small></label>
-                    <input type="number" <?= $disabled ?> value="<?= $hasil['agt_cuti'] ?>" class="form-control" id="agt_cuti" name="agt_cuti">
+                    <input type="number" <?= $disabled ?> value="<?= $hasil['agt_tpk'] ?>" class="form-control"
+                        id="agt_tpk" name="agt_tpk">
 
                 </div>
             </div>
@@ -129,8 +140,12 @@ error_reporting(0);
         <?php
         if ($hasil && $status == 'konfirmasi') {
         ?>
-            <button type="submit" value='simpan' name='approve' onclick="return window.confirm('Apakah yakin akan menyetujui laporan ini?')" class="btn btn-success"><i class="fa fa-check"></i> APPROVE</button>
-            <button type="submit" value='pending' name='reject' onclick="return window.confirm('Apakah yakin akan Reject laporan ini?')" class="btn btn-danger"><i class="fa fa-times"></i> REJECT</button>
+        <button type="submit" value='simpan' name='approve'
+            onclick="return window.confirm('Apakah yakin akan menyetujui laporan ini?')" class="btn btn-success"><i
+                class="fa fa-check"></i> APPROVE</button>
+        <button type="submit" value='pending' name='reject'
+            onclick="return window.confirm('Apakah yakin akan Reject laporan ini?')" class="btn btn-danger"><i
+                class="fa fa-times"></i> REJECT</button>
         <?php
         }
         ?>
@@ -183,22 +198,22 @@ if (isset($_POST['reject'])) {
 
 ?>
 <style>
-    /* CSS untuk input */
-    input {
-        font-weight: bold;
-        vertical-align: auto;
-        /* Membuat huruf tebal */
-        font-size: 18px;
-        /* Menentukan ukuran huruf */
-        text-align: center;
-        /* Menengahkan teks */
-    }
+/* CSS untuk input */
+input {
+    font-weight: bold;
+    vertical-align: auto;
+    /* Membuat huruf tebal */
+    font-size: 18px;
+    /* Menentukan ukuran huruf */
+    text-align: center;
+    /* Menengahkan teks */
+}
 
-    #hasil,
-    #hasil_agt,
-    #total_pemb {
-        text-align: center;
-        font-weight: bold;
-        font-size: larger;
-    }
+#hasil,
+#hasil_agt,
+#total_pemb {
+    text-align: center;
+    font-weight: bold;
+    font-size: larger;
+}
 </style>
