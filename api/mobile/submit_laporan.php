0dba268c71ed7effc65dcb115767c11a26be2a8b
rikydwianto <rikydwianto04@gmail.com>
2024-03-22 23:51:40 +0800

    asasa

--- api/mobile/submit_laporan.php
+++ api/mobile/submit_laporan.php
@@ -29,6 +29,14 @@ if (isset($_GET['akses']) && $_GET['akses'] == 'android') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SUBMIT LAPORAN</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    .scroll-box {
+        max-height: 300px;
+        overflow-y: scroll;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+    </style>
 </head>
 
 <body>
