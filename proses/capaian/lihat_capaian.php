5021036e2b8d255bf517faded0f1f5e236946412
rikydwianto <rikydwianto04@gmail.com>
2024-06-30 10:05:33 +0700

    capaian

--- proses/capaian/lihat_capaian.php
+++ proses/capaian/lihat_capaian.php
@@ -7,7 +7,7 @@
             <th>Bulan</th>
             <th>Tahun</th>
             <th>Capaian</th>
-            <th>ID User</th>
+            <th>Nama User</th>
             <th>Tanggal Input</th>
             <th>#</th>
         </tr>
@@ -17,8 +17,9 @@
         try {
 
             // Query untuk mengambil data dari tabel capaian
-            $query = "SELECT * FROM capaian where id_user='$sesi'";
-            $stmt = $pdo->query($query);
+            $query = "SELECT * FROM capaian join users on users.id=capaian.id_user where capaian.id_user=?";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$sesi]);
             $no = 1;
             // Tampilkan data dalam tabel HTML
             if ($stmt->rowCount() > 0) {
@@ -26,27 +27,29 @@
 
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         ?>
-                    <tr>
-                        <td><?= $no++ ?></td>
-                        <td><?= $row['aktifitas'] ?></td>
-                        <td><?= $row['singkatan'] ?></td>
-                        <td><?= $bulanArray[$row['bulan']] ?></td>
-                        <td><?= $row['tahun'] ?></td>
-                        <td class='text-center'><?= $row['capaian'] ?></td>
-                        <td><?= $row['id_user'] ?></td>
-                        <td><?= $row['tgl_input'] ?></td>
-                        <td>
-                            <a href="<?= $url . "index.php?menu=index&act=capaian&submenu=hapuscapaian&id=" . $row['id'] ?>" class="btn btn-danger" onclick="return window.confirm('apakah yakin untuk menghapus ini?')"><i class="fa fa-times"></i></a>
-                        </td>
-                    </tr>
-                <?php
+        <tr>
+            <td><?= $no++ ?></td>
+            <td><?= $row['aktifitas'] ?></td>
+            <td><?= $row['singkatan'] ?></td>
+            <td><?= $bulanArray[$row['bulan']] ?></td>
+            <td><?= $row['tahun'] ?></td>
+            <td class='text-center'><?= $row['capaian'] ?></td>
+            <td><?= $row['nama'] ?></td>
+            <td><?= $row['tgl_input'] ?></td>
+            <td>
+                <a href="<?= $url . "index.php?menu=index&act=capaian&submenu=hapuscapaian&id=" . $row['id'] ?>"
+                    class="btn btn-danger" onclick="return window.confirm('apakah yakin untuk menghapus ini?')"><i
+                        class="fa fa-times"></i></a>
+            </td>
+        </tr>
+        <?php
 
                 }
             } else {
                 ?>
-                <tr>
-                    <td colspan="9" class='text-center'>Tidak ada data!</td>
-                </tr>
+        <tr>
+            <td colspan="9" class='text-center'>Tidak ada data!</td>
+        </tr>
         <?php
             }
         } catch (PDOException $e) {
--- proses/target/detail_target.php
+++ proses/target/detail_target.php
@@ -4,7 +4,7 @@ $aktifitas = urldecode($_GET['aktifitas']);
 $singkatan = urldecode($_GET['singkatan']);
 $tahun = $_GET['tahun'];
 
-$stmt = $pdo->prepare("SELECT * FROM target WHERE aktifitas = ? AND singkatan = ? AND tahun = ? AND id_user = ? order by bulan");
+$stmt = $pdo->prepare("SELECT * FROM target WHERE aktifitas = ? AND singkatan = ? AND tahun = ? AND id_user = ? order by bulan asc");
 $stmt->execute([$aktifitas, $singkatan, $tahun, $sesi]);
 $daftarTarget = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (!$daftarTarget) {
@@ -25,24 +25,26 @@ if (!$daftarTarget) {
     </thead>
     <tbody>
         <?php foreach ($daftarTarget as $target) : ?>
-            <tr>
-                <td><?php echo $target['bulan'] ?></td>
-                <td><?php echo $bulanArray[$target['bulan']]; ?></td>
-                <td><?php echo $target['tahun']; ?></td>
-                <td><?php echo $target['aktifitas']; ?></td>
-                <td><?php echo $target['target']; ?></td>
-                <td>
-                    <a href="<?= $url . "index.php?menu=index&act=target&submenu=edit_target&id=" . $target['id'] ?>" class="btn btn-primary">
-                        Edit
-                    </a>
-                    <a href="<?= $url . "index.php?menu=index&act=target&submenu=hapus_target&id=" . $target['id'] . "&aktifitas=" . urlencode($target['aktifitas']) . "&singkatan=" . urlencode($target['singkatan']) . "&tahun=" . $target['tahun'] ?>" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus target ini?')">
-                        Hapus
-                    </a>
+        <tr>
+            <td><?php echo $target['bulan'] ?></td>
+            <td><?php echo $bulanArray[$target['bulan']]; ?></td>
+            <td><?php echo $target['tahun']; ?></td>
+            <td><?php echo $target['aktifitas']; ?></td>
+            <td><?php echo $target['target']; ?></td>
+            <td>
+                <a href="<?= $url . "index.php?menu=index&act=target&submenu=edit_target&id=" . $target['id'] ?>"
+                    class="btn btn-primary">
+                    Edit
+                </a>
+                <a href="<?= $url . "index.php?menu=index&act=target&submenu=hapus_target&id=" . $target['id'] . "&aktifitas=" . urlencode($target['aktifitas']) . "&singkatan=" . urlencode($target['singkatan']) . "&tahun=" . $target['tahun'] ?>"
+                    class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus target ini?')">
+                    Hapus
+                </a>
 
 
 
-                </td>
-            </tr>
+            </td>
+        </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
