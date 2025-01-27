d07fc485ed4558e550064c3be4474cd88297b023
rikydwianto <rikydwianto016@gmail.com>
2025-01-27 13:29:12 +0700

    center meeting

--- proses/center_input.php
+++ proses/center_input.php
@@ -42,35 +42,118 @@ if (isset($_POST['xml-preview'])) {
 
         $total_center = 0;
         $hitung_hari = count($hari);
-        foreach ($hari  as $day) {
-            $days = strtolower($day["HARI1"]);
-            //   echo $day['HARI1']."<br/>";
-            foreach ($day->OfficerName_Collection as $hari_staff) {
-                //   echo $day;
-                foreach ($hari_staff->OfficerName as $staff) {
-                    $nama_staff = explode("Total ", $staff['OfficerName1'])[0];
-                    //   echo $nama_staff."<br/>";
-                    foreach ($staff->CenterID_Collection->CenterID as $ctr_staf) {
-                        $no_center =  $ctr_staf['CenterID'];
-                        $center[] = $no_center;
-                        $detail_center = $ctr_staf->Details_Collection->Details;
-                        $jam = rubahkata($detail_center['MeetingTime']);
-                        $agt = $detail_center['Textbox128'];
-                        $client = $detail_center['JumlahClient'];
-                        $desa = aman(ganti_karakter(rubahkata($detail_center['DusunName'])));
-                        $kecamatan = aman(ganti_karakter(rubahkata($detail_center['KecamatanName'])));
-                        $kab = aman(ganti_karakter(rubahkata($detail_center['KabupatenName'])));
-                        $qtxt = "INSERT INTO 
-                        `center` (`id_center`, `no_center`, `doa_center`, `hari`, `status_center`, `member_center`, `anggota_center`, `center_bayar`, `id_cabang`, `id_karyawan`, `id_laporan`, `jam_center`, `latitude`, `longitude`, `doortodoor`, `blacklist`, `konfirmasi`, `staff`,desa,kecamatan,kabupaten,anggota_hadir,nama_cabang) 
-                        VALUES (NULL, '$no_center', 'y', '$days', 'hijau', '$agt', '$client', '$client', '$sesi', '0', '0', '$jam', 'null', 'null', 't', 't', 't', '$nama_staff','$desa','$kecamatan','$kab','$agt','$namaCabang'); 
-                        ";
-                        $pdo->query($qtxt);
 
-                        $total_center++;
+        foreach ($hari as $day) {
+            // Ambil nama hari dan ubah menjadi huruf kecil
+            $days = strtolower($day["HARI1"] ?? '');
+
+            foreach ($day->OfficerName_Collection as $hari_staff) {
+                foreach ($hari_staff->OfficerName as $staff) {
+                    // Ambil nama staff dengan memisahkan string berdasarkan "Total"
+                    $nama_staff = explode("Total ", $staff['OfficerName1'] ?? '')[0];
+
+
+                    foreach ($staff->CenterID_Collection->CenterID  as $ctr_staf) {
+                        foreach ($ctr_staf->CenterName as $center) {
+                            // Ambil ID Center
+                            $no_center = $ctr_staf['CenterID'] ?? '';
+
+                            // Ambil detail center
+                            $detail_center = $center->Details_Collection->Details ?? null;
+                            // var_dump($detail_center);
+                            // Ambil atribut dari detail
+                            $jam = rubahkata($detail_center['MeetingTime'] ?? '');
+                            $agt = $detail_center['Textbox128'] ?? '0';
+                            $client = $detail_center['JumlahClient'] ?? '0';
+                            $desa = aman(ganti_karakter(rubahkata($detail_center['DusunName'] ?? '')));
+                            $kecamatan = aman(ganti_karakter(rubahkata($detail_center['KecamatanName'] ?? '')));
+                            $kab = aman(ganti_karakter(rubahkata($detail_center['KabupatenName'] ?? '')));
+                            // Siapkan query SQL
+                            $qtxt = "
+                            INSERT INTO `center` (
+                                `id_center`,
+                                `no_center`,
+                                `doa_center`,
+                                `hari`,
+                                `status_center`,
+                                `member_center`,
+                                `anggota_center`,
+                                `center_bayar`,
+                                `id_cabang`,
+                                `id_karyawan`,
+                                `id_laporan`,
+                                `jam_center`,
+                                `latitude`,
+                                `longitude`,
+                                `doortodoor`,
+                                `blacklist`,
+                                `konfirmasi`,
+                                `staff`,
+                                `desa`,
+                                `kecamatan`,
+                                `kabupaten`,
+                                `anggota_hadir`,
+                                `nama_cabang`
+                            ) VALUES (
+                                NULL,
+                                :no_center,
+                                'y',
+                                :days,
+                                'hijau',
+                                :agt,
+                                :client,
+                                :client_bayar,
+                                :id_cabang,
+                                '0',
+                                '0',
+                                :jam,
+                                NULL,
+                                NULL,
+                                't',
+                                't',
+                                't',
+                                :nama_staff,
+                                :desa,
+                                :kecamatan,
+                                :kabupaten,
+                                :anggota_hadir,
+                                :nama_cabang
+                            );
+                        ";
+
+                            // Debug query jika diperlukan
+                            // echo $qtxt . '<br>';
+
+                            // Eksekusi query menggunakan prepared statements
+                            $arr =  [
+                                ':no_center' => $no_center,
+                                ':days' => $days,
+                                ':agt' => $agt,
+                                ':client' => $client,
+                                ':client_bayar' => $client,
+                                ':id_cabang' => $sesi,
+                                ':jam' => $jam,
+                                ':nama_staff' => $nama_staff,
+                                ':desa' => $desa,
+                                ':kecamatan' => $kecamatan,
+                                ':kabupaten' => $kab,
+                                ':anggota_hadir' => $agt,
+                                ':nama_cabang' => $namaCabang,
+                            ];
+                            $stmt = $pdo->prepare($qtxt);
+                            $stmt->execute($arr);
+                            // var_dump($arr);
+                            // Tambahkan ke total center
+                            $total_center++;
+                        }
                     }
                 }
             }
         }
+
+
+        echo "Total center processed: $total_center";
+
 
 
         pindah("index.php?menu=center_proses&nama_cabang=$namaCabang");
